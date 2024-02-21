4a5a7b822a9bf6aed2880cec253b446e7d9c7f96
Mincheol Chae <cmc752@gmail.com>
2024-02-21 17:32:21 +0900

    init

--- src/main/java/kr/co/barfdog/printsticker/controller/PrintController.java
+++ src/main/java/kr/co/barfdog/printsticker/controller/PrintController.java
@@ -1,2 +1,137 @@
-package kr.co.barfdog.printsticker;public class PrintController {
+package kr.co.barfdog.printsticker.controller;
+
+import kr.co.barfdog.printsticker.model.ExcelData;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.tika.exception.TikaException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class PrintController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @PostMapping("/label")
+    public String readExcel(@RequestParam("file") MultipartFile file, Model model) throws TikaException, IOException { // 2
+//        List<ExcelData> dataList = new ArrayList<>();
+//
+//        try (InputStream is = file.getInputStream();) {
+//
+//            Tika tika = new Tika();
+//            String mimeType = tika.detect(is);
+//            if (isAllowedMIMEType(mimeType)) {
+//                Workbook workbook = new XSSFWorkbook(file.getInputStream());
+//
+//                Sheet worksheet = workbook.getSheetAt(0);
+//
+//                String atchFileId = null;
+//
+//                for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) { // 1번째 행부터 끝까지
+//                    Row row = worksheet.getRow(i);
+//
+//                    ExcelData data = new ExcelData();
+//                    data.setNum((int) row.getCell(0).getNumericCellValue());
+//                    data.setName(row.getCell(1).getStringCellValue());
+//
+//                    dataList.add(data);
+//                }
+//
+//                model.addAttribute("list", dataList);
+//            } else {
+//                throw new IOException();
+//            }
+//        } catch (Exception e) {
+//            throw new TikaException("ERROR");
+//        }
+        List<ExcelData> dataList = new ArrayList<>();
+
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename()); // 3
+
+        if (!extension.equals("xlsx") && !extension.equals("xls")) {
+            throw new IOException("엑셀파일만 업로드 해주세요.");
+        }
+
+        Workbook workbook = null;
+
+        if (extension.equals("xlsx")) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        } else if (extension.equals("xls")) {
+            workbook = new HSSFWorkbook(file.getInputStream());
+        }
+
+        Sheet worksheet = workbook.getSheetAt(0);
+
+        for (int i = 1; i < worksheet.getPhysicalNumberOfRows(); i++) { // 4
+
+            Row row = worksheet.getRow(i);
+
+            ExcelData data = new ExcelData();
+
+            int index = (int) row.getCell(0).getNumericCellValue();
+            if (index == 0) {
+                break;
+            }
+            data.setIndex(index);
+            data.setMerchantUid(row.getCell(1).getStringCellValue());
+            data.setOrderStatus(row.getCell(2).getStringCellValue());
+            data.setSubscribeStatus(row.getCell(3).getStringCellValue());
+            data.setMemberName(row.getCell(5).getStringCellValue());
+            data.setDeliveryName(row.getCell(6).getStringCellValue());
+            data.setDogType(row.getCell(7).getStringCellValue());
+            data.setDogName(row.getCell(8).getStringCellValue());
+            data.setDogBirth((int) row.getCell(29).getNumericCellValue());
+            data.setDogGender(row.getCell(28).getStringCellValue());
+            data.setDogSize(row.getCell(31).getStringCellValue());
+            data.setDogNeutralization(row.getCell(33).getStringCellValue());
+            data.setDogWeight(row.getCell(32).getNumericCellValue());
+            data.setDogStatus(row.getCell(37).getStringCellValue());
+            data.setDogActivityLevel(row.getCell(34).getStringCellValue());
+            data.setDogSnackCountLevel(row.getCell(38).getStringCellValue());
+            data.setDogWalkingCountPerWeek((int) row.getCell(35).getNumericCellValue());
+            data.setDogWalkingTimePerOneTime(row.getCell(36).getNumericCellValue());
+            data.setDogInedibleFood(row.getCell(39).getStringCellValue());
+            data.setDogInedibleFoodEtc(row.getCell(40).getStringCellValue());
+            data.setDogCaution(row.getCell(41).getStringCellValue());
+            data.setStarterPremium(row.getCell(13).getNumericCellValue());
+            data.setTurkeyAndBeef(row.getCell(14).getNumericCellValue());
+            data.setDuckAndLamb(row.getCell(15).getNumericCellValue());
+            data.setLambAndBeef(row.getCell(16).getNumericCellValue());
+            data.setNumOfPacks((int) row.getCell(10).getNumericCellValue());
+            data.setDeliveryInterval((int) row.getCell(24).getNumericCellValue());
+
+            dataList.add(data);
+        }
+
+        model.addAttribute("datas", dataList); // 5
+
+
+        return "frame-1";
+    }
+
+
+    private boolean isAllowedMIMEType(String mimeType) {
+        if (mimeType.equals("application/x-tika-ooxml"))
+            return true;
+        return false;
+    }
+
+
 }
--- src/main/java/kr/co/barfdog/printsticker/model/ExcelData.java
+++ src/main/java/kr/co/barfdog/printsticker/model/ExcelData.java
@@ -1,2 +1,42 @@
-package kr.co.barfdog.printsticker.model;public class ExcelData {
+package kr.co.barfdog.printsticker.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ExcelData {
+
+    private int index;
+    private String merchantUid;  //주문번호
+    private String orderStatus;  //주문상태
+    private String subscribeStatus;  //구독상태
+    private String memberName;  //견주
+    private String deliveryName;  //수령자
+    private String dogType;  //견종
+    private String dogName;  //견명
+    private int numOfPacks;  //팩수
+    private double starterPremium;  //SP
+    private double turkeyAndBeef;  //TB
+    private double duckAndLamb;  //DL
+    private double lambAndBeef;  //LB
+    private int deliveryInterval;  //배송주기(일)
+    private String dogGender;  //견 성별
+    private int dogBirth;  //견 출생년월
+    private String dogSize;  // 소,중,대
+    private double dogWeight;  //견 무게
+    private String dogNeutralization;  //중성화 여부
+    private String dogActivityLevel;  //견 활동량
+    private int dogWalkingCountPerWeek;  //견 산책량(1주당 몇 회)
+    private double dogWalkingTimePerOneTime;  //견 산책량(1회당 몇 시간)
+    private String dogStatus;  //견 현재상태
+    private String dogSnackCountLevel;  //견 간식량
+    private String dogInedibleFood;  //못먹는 음식
+    private String dogInedibleFoodEtc;  //못먹는 음식 기타
+    private String dogCaution; //기타 질병
+
+
+
 }
